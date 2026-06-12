545b95e7b36b4b6538ddf97406fd437995c85cfb
nikitsya <nikitasmiichyk@gmail.com>
2026-06-12 23:58:45 +0100

    Fix price endpoint to return price data

--- src/main/java/com/nikitsya/billing/price/repository/PriceRepository.java
+++ src/main/java/com/nikitsya/billing/price/repository/PriceRepository.java
@@ -1,11 +1,10 @@
 package com.nikitsya.billing.price.repository;
 
-import com.nikitsya.billing.customer.model.Customer;
 import com.nikitsya.billing.price.model.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PriceRepository extends JpaRepository<Customer, Long> {
+public interface PriceRepository extends JpaRepository<Price, Long> {
     List<Price> getAllPrices();
 }
